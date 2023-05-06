5c343d20f6bfb2bb7ba4ad673b5dbb182b2ad45b
Daniel Rahmi <50299064+rahmidaniel@users.noreply.github.com>
2023-05-06 17:53:42 +0200

    Last article now shows when game ended.

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -7,11 +7,13 @@ import EndModal from "./EndModal";
 const MainContent = () => {
     const prop = useContext(ArticleContext);
 
+    const wiki = <WikiArticle/>;
+
     // Dictionary like storage for state specific JSX components
     const components = {
         [AppState.MENU]: <p className="m-auto">[should show rules and guide]</p>,
-        [AppState.STARTED]: <WikiArticle/>,
-        [AppState.ENDED]:  <EndModal/>
+        [AppState.STARTED]: wiki,
+        [AppState.ENDED]:  <>{wiki}<EndModal/></>
     };
 
     return(components[prop.appState]);
