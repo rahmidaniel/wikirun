691820ee2915cf29c020ccbf49996547e9d9e0e5
Daniel Rahmi <50299064+rahmidaniel@users.noreply.github.com>
2023-05-14 14:58:23 +0200

    Added batch callback option as to increase performance.

--- src/api/links/fetchLinks.ts
+++ src/api/links/fetchLinks.ts
@@ -4,11 +4,12 @@ import axios from "axios";
 
 /**
  * Calls the Wikipedia API to fetch links for a given page.
- * @param pageTitle - The name of the page to fetch links for.
- * @param reverse - If `true`, loads links that lead to a page. If `false`, loads links on a page. The default is `false`.
- @returns A Promise that resolves to an array of link titles.
+ * @param pageTitle - Name of the page to fetch links for.
+ * @param reverse - If `true`, loads links that lead to a page. If `false`, loads links on a page. The **default** is `false`.
+ * @param batchCallback - *optional* - calls given function with a partial fetched list. Useful if the page contains a lot of links.
+ * @returns A Promise that resolves to an array of string titles.
  */
-export const fetchLinks = async (pageTitle: string, reverse: boolean = false): Promise<string[]> => {
+export const fetchLinks = async (pageTitle: string, reverse: boolean = false, batchCallback?: (partialMatch: string[])=>void): Promise<string[]> => {
     let matches: string[] = [];
     let moreLinks = true;
 
@@ -22,32 +23,37 @@ export const fetchLinks = async (pageTitle: string, reverse: boolean = false): P
                 format: "json",
                 formatversion: "2",
                 origin: "*",
-                disableeditsection: true,
-                disabletoc: true,
                 redirects: true,
                 titles: decodeURIComponent(pageTitle),
                 // Defines the data we need => LINKS (can limit the number of links 1 to 500 or "max")
                 ...(reverse ?
-                    {prop: "linkshere", lhlimit: "10", lhnamespace: "0"} :
-                    {prop: "links", pllimit: "10", plnamespace: "0"}),
+                    {prop: "linkshere", lhlimit: "max", lhnamespace: "0"} :
+                    {prop: "links", pllimit: "max", plnamespace: "0"}),
                 // Continuation point needs to be set
                 ...(continueValue ? { [ reverse ? "lhcontinue" : "plcontinue"]: continueValue} : {}),
             }
-        }).then((response) => {
+        }).then((response)=> {
+            // Container for batching
+            const batch: string[] = [];
             if(reverse) {
                 const queryResult = (response.data as (ReLinksResult));
                 // Adding fetched data to the list of matches
-                matches.push(...queryResult.query.pages[0].linkshere.map(page => page.title));
+                batch.push(...queryResult.query.pages[0].linkshere.map(page => page.title));
                 // Setting continuation point if further calls are needed
                 continueValue = queryResult.continue !== undefined ? queryResult.continue.lhcontinue : null;
-
             } else { // Same as above, but different response needs different types
                 const queryResult = (response.data as (LinksResult));
-                matches.push(...queryResult.query.pages[0].links.map(page => page.title));
+                batch.push(...queryResult.query.pages[0].links.map(page => page.title));
                 continueValue = queryResult.continue !== undefined ? queryResult.continue.plcontinue : null;
             }
             // Check if there are any more links to fetch
             moreLinks = !!continueValue;
+
+            // Fire callback function with batch
+            batchCallback?.(batch);
+
+            // Add batch to matches
+            matches.push(...batch);
         }).catch((error) => {
             if (!axios.isCancel(error)) console.log(`Error in query: ${pageTitle}`, error);
 
